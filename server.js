8936f071d36822c2d6895dccae7c144ac5096c39
ArthurSandim <avg.sandim@gmail.com>
2026-05-13 16:04:24 -0300

    feat: add MongoDB connection with Mongoose

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import 'dotenv/config';
 import app from './src/app.js';
+import connectDB from './src/config/db.js'
+
+connectDB();
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor rodando na porta ${process.env.PORT}`)
