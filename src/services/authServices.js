12e34ec0349e815ec0c8a5342ef758842b9b6b68
ArthurSandim <avg.sandim@gmail.com>
2026-05-17 19:34:12 -0300

    feat: add register and login with JWT authentication

---
+++ src/services/authServices.js
@@ -0,0 +1,30 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import Professional from '../models/Professional.js';
+
+
+export const userCreator = async (name, email, password) => {
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const newUser = await Professional.create({ name, email, password: hashedPassword });
+    const { password: _, ...userWithoutPassword} = newUser.toObject();
+    return userWithoutPassword;
+};
+
+export const userAuthenticator = async (email, password) => {
+    const user = await Professional.findOne({ email });
+
+    if (!user) {
+        throw new Error('Email ou senha inválidos');
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+
+    if (!isPasswordValid) {
+        throw new Error('Email ou senha inválidos');
+    }
+
+    const token = jwt.sign({id: user.id}, process.env.JWT_SECRET, { expiresIn: '1d' });
+
+    return token;
+
+}
