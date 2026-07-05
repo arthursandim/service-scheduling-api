c95a413b9329d7f129a2a0a9fdb51aef1247d6d9
ArthurSandim <avg.sandim@gmail.com>
2026-07-05 09:55:27 -0300

    chore: add start:test script and NODE_ENV-based db connection for E2E tests

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,8 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGODB_URI);
+    const uri = process.env.NODE_ENV === 'test' ? process.env.MONGODB_URI_TEST : process.env.MONGODB_URI;
+    await mongoose.connect(uri);
     console.log('MongoDB conectado!');
   } catch (erro) {
     console.log('Erro ao conectar no MongoDB: ' + erro.message);
